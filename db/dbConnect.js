30969043bdc1e264d74f99fc6924161d12a79a24
V1GENIUS <Vivekrathore9111@gmail.com>
2024-11-12 17:39:46 +0530

    finels commit

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
-require('dotenv').config();  
+require('dotenv').config();
 
-const mongoURI = process.env.MONGO_URL || 'mongodb://localhost:27017/server';
+const mongoURI = process.env.MONGO_URL;
+
+if (!mongoURI) {
+    console.error('Error: MONGO_URL is not defined in the environment variables.');
+    process.exit(1); // Exit if the URI is missing
+}
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase timeout
+    serverSelectionTimeoutMS: 30000, // Increase server selection timeout
 })
     .then(() => {
         console.log('MongoDB Connected Successfully');
@@ -14,4 +21,36 @@ mongoose.connect(mongoURI, {
         console.error('Error connecting to MongoDB:', error);
     });
 
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
 module.exports = mongoose;
+
+// const mongoose = require('mongoose');
+// require('dotenv').config();  
+
+// const mongoURI = process.env.MONGO_URL || 'mongodb://localhost:27017/server';
+
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     connectTimeoutMS: 30000, // 30 seconds
+//     serverSelectionTimeoutMS: 30000,
+// })
+//     .then(() => {
+//         console.log('MongoDB Connected Successfully');
+//     })
+//     .catch((error) => {
+//         console.error('Error connecting to MongoDB:', error);
+//     });
+
+// module.exports = mongoose;
